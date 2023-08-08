04bcc14f523c9b7900f7a5ed60195b51e4184eae
krasew <endless.universe@bk.ru>
2023-08-08 10:52:06 +0300

    3.1.1

--- app/src/main/java/com/bebwhepan/app/Models/Taco/Taco.java
+++ app/src/main/java/com/bebwhepan/app/Models/Taco/Taco.java
@@ -1,5 +1,6 @@
 package com.bebwhepan.app.Models.Taco;
 
+import java.util.Date;
 import java.util.List;
 
 import jakarta.validation.constraints.NotNull;
@@ -8,11 +9,14 @@ import lombok.Data;
 
 @Data
 public class Taco {
+    private Long id;
+    private Date createdAt = new Date();
+
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient") 
-    private List<IngredientTaco> ingredients;
+    private List<IngredientTaco> ingredients; 
 }
--- app/src/main/java/com/bebwhepan/app/Models/Taco/TacoOrder.java
+++ app/src/main/java/com/bebwhepan/app/Models/Taco/TacoOrder.java
@@ -1,15 +1,26 @@
 package com.bebwhepan.app.Models.Taco;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.CreditCardNumber;
-import java.util.List;
-import java.util.ArrayList;
+
 import lombok.Data;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private Date placedAt;
+
     @NotBlank(message="Delivery name is required")
     private String deliveryName;
 
