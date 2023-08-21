76fefd572f242991fa966659bffc126a53d151bb
krasew <endless.universe@bk.ru>
2023-08-21 10:35:09 +0300

    changes in TacoOrder; org.springframework isnt working, that's why i created Table.java and Id.java

--- app/src/main/java/com/bebwhepan/app/Models/Taco/TacoOrder.java
+++ app/src/main/java/com/bebwhepan/app/Models/Taco/TacoOrder.java
@@ -10,13 +10,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
+// import org.springframework.data.annotation.Id;
+//import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
 
 @Data
+@Table
 public class TacoOrder implements Serializable{
     private static final long serialVersionUID = 1L;
 
+    @Id
     private Long id;
 
     private Date placedAt;
