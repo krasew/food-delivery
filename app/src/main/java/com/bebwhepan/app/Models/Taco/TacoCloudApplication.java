e67228bcf4d6985d710d241af73065e5662ed9e5
krasew <endless.universe@bk.ru>
2023-08-22 14:48:18 +0300

    TacoCloudApplication created

---
+++ app/src/main/java/com/bebwhepan/app/Models/Taco/TacoCloudApplication.java
@@ -0,0 +1,32 @@
+package com.bebwhepan.app.Models.Taco;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.bebwhepan.app.Models.Taco.IngredientTaco.Type;
+import com.bebwhepan.app.Data.IngredientRepository;
+
+public class TacoCloudApplication {
+    public static void main(String[] args) {
+            SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+    return args -> {
+      repo.deleteAll(); // TODO: Quick hack to avoid tests from stepping on each other with constraint violations
+      repo.save(new IngredientTaco("FLTO", "Flour Tortilla", Type.WRAP));
+      repo.save(new IngredientTaco("COTO", "Corn Tortilla", Type.WRAP));
+      repo.save(new IngredientTaco("GRBF", "Ground Beef", Type.PROTEIN));
+      repo.save(new IngredientTaco("CARN", "Carnitas", Type.PROTEIN));
+      repo.save(new IngredientTaco("TMTO", "Diced Tomatoes", Type.VEGGIES));
+      repo.save(new IngredientTaco("LETC", "Lettuce", Type.VEGGIES));
+      repo.save(new IngredientTaco("CHED", "Cheddar", Type.CHEESE));
+      repo.save(new IngredientTaco("JACK", "Monterrey Jack", Type.CHEESE));
+      repo.save(new IngredientTaco("SLSA", "Salsa", Type.SAUCE));
+      repo.save(new IngredientTaco("SRCR", "Sour Cream", Type.SAUCE));
+    };
+  }
+}
